e26c62f5ada8ec08f3789cf548b194d26fb25e4f
k32 <10274441+k32@users.noreply.github.com>
2019-08-12 01:21:43 +0200

    CLI documentation

--- test/lee_cli_tests.erl
+++ test/lee_cli_tests.erl
@@ -1,5 +1,7 @@
 -module(lee_cli_tests).
 
+-export([test_model_raw/0, test_model/0]).
+
 -include_lib("lee/src/framework/lee_internal.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
@@ -10,26 +12,31 @@ test_cli_params() ->
            , #{ cli_operand => "long"
               , type => typerefl:string()
               , default => "default"
+              , oneliner => "An example of a long CLI argument"
               }
            }
      , short => {[value, cli_param]
                 , #{ cli_short => "s"
                    , type => typerefl:integer()
+                   , oneliner => "An example of a short CLI argument"
                    }
                 }
      , flag1 => {[value, cli_param]
                 , #{ cli_short => "f"
                    , type => typerefl:boolean()
+                   , oneliner => "Flag1"
                    }
                 }
      , flag2 => {[value, cli_param]
                 , #{ cli_short => "g"
                    , type => typerefl:boolean()
+                   , oneliner => "Some flag"
                    }
                 }
      , flag3 => {[value, cli_param]
                 , #{ cli_short => "h"
                    , type => typerefl:boolean()
+                   , oneliner => "Another flag"
                    }
                 }
      , both =>
@@ -37,6 +44,7 @@ test_cli_params() ->
            , #{ cli_operand => "both"
               , cli_short => "b"
               , type => typerefl:tuple([foo, typerefl:integer()])
+              , oneliner => "This can be set via short or long argument"
               }
            }
      }.
@@ -49,6 +57,12 @@ test_cli_action(Name, KeyElems, Children) ->
     {[map, cli_action]
     , #{ cli_operand => Name
        , ?key_elements => KeyElems
+       , oneliner => "This is a test CLI action. It does stuff"
+       , doc => "<para>This is a long and elaborate
+                 documentation of a CLI action</para>
+                 <para>
+                 Blah blah blah
+                 </para>"
        }
     , lists:foldl(fun maps:merge/2, #{}, Children)
     }.
@@ -77,23 +91,26 @@ test_positional_args2() ->
            }
      }.
 
+test_model_raw() ->
+    #{ foo => test_cli_params()
+     , action_1 =>
+           test_cli_action("action_1", [[short], [long]]
+                          , [test_cli_params()])
+     , action_2 =>
+           test_cli_action("action_2", [[posn_1]]
+                          , [test_positional_args1()])
+     , action_3 =>
+           test_cli_action("action_3", []
+                          , [test_positional_args2()])
+     , action_4 =>
+           test_cli_action("action_4", [[posn_1]]
+                          , [ test_positional_args1()
+                            , test_positional_args2()
+                            ])
+     }.
+
 test_model() ->
-    MF = #{ foo => test_cli_params()
-          , action_1 =>
-                test_cli_action("action_1", [[short], [long]]
-                               , [test_cli_params()])
-          , action_2 =>
-                test_cli_action("action_2", [[posn_1]]
-                               , [test_positional_args1()])
-          , action_3 =>
-                test_cli_action("action_3", []
-                               , [test_positional_args2()])
-          , action_4 =>
-                test_cli_action("action_4", [[posn_1]]
-                               , [ test_positional_args1()
-                                 , test_positional_args2()
-                                 ])
-          },
+    MF = test_model_raw(),
     {ok, M} = lee_model:compile([], [MF]),
     M.
 
